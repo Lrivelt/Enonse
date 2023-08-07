168755f5d000816e0cf64d3bd5f0a1c6d4476f1d
Lrivelt <113407898+Lrivelt@users.noreply.github.com>
2023-08-07 18:15:57 -0400

    Add files via upload

---
+++ boutik.sql
@@ -0,0 +1,8 @@
+Create database boutik character set 'utf8mb4';
+Use boutik;
+Create table Pwodui (id_pwod int not null, non_pwod varchar (36) not null, dat_ekspirasyon date, );
+
+
+Create table kategori (vetman_fanm varchar (38) not null, enfomatik varchar(45) not null, konprime_doule varchar(30));
+
+Create table kliyan (Id_kliyan int not null, non varchar(30) not null, siyati varchar(36), imel varchar (30));
